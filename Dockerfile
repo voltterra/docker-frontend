bc3d2ccae69162272bb8e8bce2023763e67d52ec
Dmitry Aminev <dmitry.aminev@wonttellyou.com>
2022-08-02 13:15:13 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# build stage
+FROM node:alpine as builder
+WORKDIR '/app'
+RUN chown -R node:node /app
+COPY --chown=node:node package.json .
+RUN npm install
+COPY --chown=node:node . .
+RUN npm run build
+
+# run stage
+FROM nginx
+COPY --from=builder --chown=nginx:nginx /app/build /usr/share/nginx/html
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          To get started with the platform <code>src/App.js</code> and save to reload.
+          Let's test our dev capabilities here and see how the changes are reflected elsewhere
+        </p>
+        <p>I was changed</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
